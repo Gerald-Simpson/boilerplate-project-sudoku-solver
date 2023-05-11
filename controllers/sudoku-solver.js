040086d8de9abe789a7c3c8cda5b6ce6e8548db1
Gerald-Simpson <gtnsimpson@gmail.com>
2023-05-11 09:27:27 +0100

    Code executes correctly, but this method does not successfully solve a sudoku. Will need to read about sudoku solution algorithms.

--- controllers/sudoku-solver.js
+++ controllers/sudoku-solver.js
@@ -7,9 +7,40 @@ class SudokuSolver {
       return false;
     }
   }
+  rowColumnToStringPosition(row, column) {
+    return column - 1 + (row - 1) * 9;
+  }
+  stringPositionToColumn(stringPosition) {
+    return (stringPosition % 9) + 1;
+  }
+  stringPositionToRow(stringPosition) {
+    return Math.floor(stringPosition / 9) + 1;
+  }
+  stringPositionToRegion(stringPosition) {
+    //Dict of region string positions
+    let regionPosDict = {
+      1: [0, 1, 2, 9, 10, 11, 18, 19, 20],
+      2: [3, 4, 5, 12, 13, 14, 21, 22, 23],
+      3: [6, 7, 8, 15, 16, 17, 24, 25, 26],
+      4: [27, 28, 29, 36, 37, 38, 45, 46, 47],
+      5: [30, 31, 32, 39, 40, 41, 48, 49, 50],
+      6: [33, 34, 35, 42, 43, 44, 51, 52, 53],
+      7: [54, 55, 56, 63, 64, 65, 72, 73, 74],
+      8: [57, 58, 59, 66, 67, 68, 75, 76, 77],
+      9: [60, 61, 62, 69, 70, 71, 78, 79, 80],
+    };
+
+    // region position of strinPosition
+    for (const [key, val] of Object.entries(regionPosDict)) {
+      if (val.includes(stringPosition)) {
+        return Number(key);
+      }
+    }
+  }
+
   checkRowPlacement(puzzleString, row, column, value) {
     // Create a string of only the row
-    let rowString = puzzleString.substr(row * 9 - 9, row * 9 - 1);
+    let rowString = puzzleString.slice(row * 9 - 9, row * 9);
     //Check if any other numbers in the rowString match the value
     if (rowString.includes(String(value))) {
       // false if number does not work
@@ -23,9 +54,9 @@ class SudokuSolver {
   checkColPlacement(puzzleString, row, column, value) {
     //Create a string of only the column
     let colArr = [];
-    for (let i = 0; i < puzzleString.length; i++) {
+    for (let i = 0; i < 81; i++) {
       if (i % 9 === column - 1) {
-        colArr.push(puzzleString[i]);
+        colArr.push(puzzleString.slice(i, i + 1));
       }
     }
     let colString = colArr.join('');
@@ -52,19 +83,10 @@ class SudokuSolver {
       8: [57, 58, 59, 66, 67, 68, 75, 76, 77],
       9: [60, 61, 62, 69, 70, 71, 78, 79, 80],
     };
-
-    // string position of guess
-    let guessPosition = column - 1 + (row - 1) * 9;
-
-    // region position of guess
-    let guessRegion = 0;
-    for (const [key, val] of Object.entries(regionPosDict)) {
-      if (val.includes(guessPosition)) {
-        guessRegion = Number(key);
-      }
-    }
-
     //Create an array of the string of only the region
+    let guessRegion = this.stringPositionToRegion(
+      this.rowColumnToStringPosition(row, column)
+    );
     let regionArr = [];
     regionPosDict[guessRegion].forEach((num) => {
       regionArr.push(puzzleString[num]);
@@ -80,7 +102,73 @@ class SudokuSolver {
     }
   }
 
-  solve(puzzleString) {}
+  solve(puzzleString) {
+    // check puzzle string is valid
+    if (!this.validate(puzzleString)) {
+      return 'invalid puzzle string';
+    }
+    // create array of puzzle string
+    let solvingArr = puzzleString.split('');
+    //while (solvingArr.includes('.')) {
+    for (let i = 0; i < 100; i++) {
+      for (let i = 1; i < 10; i++) {
+        let solvingString = solvingArr.join('');
+        console.log(solvingString);
+        let possiblePositionList = [];
+        for (let k = 0; k < 81; k++) {
+          if (Number.isInteger(solvingArr[k])) {
+            continue;
+          }
+          // create list of all the string positions where i could be placed
+          let theRow = this.stringPositionToRow(k);
+          let theCol = this.stringPositionToColumn(k);
+          let theReg = this.stringPositionToRegion(k);
+          if (
+            this.checkColPlacement(solvingString, theRow, theCol, i) &
+            this.checkRowPlacement(solvingString, theRow, theCol, i) &
+            this.checkRegionPlacement(solvingString, theRow, theCol, i)
+          ) {
+            possiblePositionList.push([k, theRow, theCol, theReg, i]);
+          }
+        }
+        //console.log(possiblePositionList);
+        for (let x = 1; x < 10; x++) {
+          //check possiblePositionList for any rows with only one possibility
+          let possibleRowList = possiblePositionList.filter(
+            (position) => position[1] === x
+          );
+          if (possibleRowList.length === 1) {
+            // Replace the solvingArr element with correct answer
+            //console.log(possibleRowList);
+            solvingArr[possibleRowList[0][0]] = String(i);
+          }
+          //check possiblePositionList for any columns with only one possibility
+          let possibleColList = possiblePositionList.filter(
+            (position) => position[2] === x
+          );
+          if (possibleColList.length === 1) {
+            // Replace the solvingArr element with correct answer
+            //console.log(possibleColList);
+            solvingArr[possibleColList[0][0]] = String(i);
+          }
+          //check possiblePositionList for any regions with only one possibility
+          let possibleRegList = possiblePositionList.filter(
+            (position) => position[3] === x
+          );
+          if (possibleRegList.length === 1) {
+            // Replace the solvingArr element with correct answer
+            console.log(possibleRegList);
+            console.log(solvingArr[possibleRegList[0][0]]);
+            solvingArr[possibleRegList[0][0]] = String(i);
+            console.log(solvingArr[possibleRegList[0][0]]);
+          }
+        }
+        //console.log('next num');
+        //console.log(solvingArr.join(''));
+      }
+    }
+    return solvingArr.join('');
+  }
 }
 
 module.exports = SudokuSolver;
--- routes/api.js
+++ routes/api.js
@@ -6,14 +6,19 @@ const SudokuSolver = require('../controllers/sudoku-solver.js');
 const testPuzzle =
   '..9..5.1.85.4....2432......1...69.83.9.....6.62.71...9......1945....4.37.4.3..6..';
 
+const testPuzzleTwo =
+  '1.5..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37.';
+
 module.exports = function (app) {
   let solver = new SudokuSolver();
 
   app.route('/test').get((req, res) => {
     //res.send([solver.validate(testPuzzle), testPuzzle]);
-    //res.send([solver.checkRowPlacement(testPuzzle, 1, 1, 1), testPuzzle]);
-    //res.send([solver.checkColPlacement(testPuzzle, 1, 1, 2), testPuzzle]);
-    res.send([solver.checkRegionPlacement(testPuzzle, 1, 4, 1), testPuzzle]);
+    //res.send([solver.checkRowPlacement(testPuzzle, 1, 1, 2), testPuzzle]);
+    //res.send([solver.checkColPlacement(testPuzzle, 2, 1, 2), testPuzzle]);
+    //res.send([solver.checkRegionPlacement(testPuzzle, 1, 4, 1), testPuzzle]);
+    //res.send([solver.solve(testPuzzle), testPuzzle]);
+    res.send([solver.solve(testPuzzleTwo), testPuzzleTwo]);
   });
 
   app.route('/api/check').post((req, res) => {});
